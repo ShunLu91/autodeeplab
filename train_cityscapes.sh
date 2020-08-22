07132f9bacc79c5d36b93174217e846f37ff3ea6
lushun_mac <470651748@qq.com>
2020-08-22 14:58:07 +0800

    ad0_s

--- train_cityscapes.sh
+++ train_cityscapes.sh
@@ -1,3 +1,11 @@
-CUDA_VISIBLE_DEVICES=6 python train_autodeeplab.py \
- --batch-size 2 --dataset cityscapes --checkname July29_newbetas_branch \
- --alpha_epoch 20 --filter_multiplier 8 --resize 512 --crop_size 321
+if [ ! -d logdir  ];then
+  mkdir logdir
+fi
+
+#CUDA_VISIBLE_DEVICES=6 python train_autodeeplab.py \
+# --batch-size 2 --dataset cityscapes --checkname July29_newbetas_branch \
+# --alpha_epoch 20 --filter_multiplier 8 --resize 512 --crop_size 321
+
+CUDA_VISIBLE_DEVICES=6 nohup python -u train_autodeeplab.py --batch-size 4 --dataset cityscapes \
+ --checkname July29_newbetas_branch --alpha_epoch 20 --filter_multiplier 8 --resize 512 --crop_size 321  > ../logdir/ad0_s.log  2>&1 &
+
