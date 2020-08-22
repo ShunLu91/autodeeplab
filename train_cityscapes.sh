15d36cc4e744dd83478547911865ca135514baa8
lushun_mac <470651748@qq.com>
2020-08-22 14:11:08 +0800

    dist/test_loader=None/gpu/amp/gpu-ids

--- train_cityscapes.sh
+++ train_cityscapes.sh
@@ -1,3 +1,3 @@
-CUDA_VISIBLE_DEVICES=3 python train_autodeeplab.py \
+CUDA_VISIBLE_DEVICES=2,3 python train_autodeeplab.py \
  --batch-size 2 --dataset cityscapes --checkname July29_newbetas_branch \
- --alpha_epoch 20 --filter_multiplier 8 --resize 512 --crop_size 321 --use_amp
+ --alpha_epoch 20 --filter_multiplier 8 --resize 512 --crop_size 321 --gpu-ids [0, 1]
