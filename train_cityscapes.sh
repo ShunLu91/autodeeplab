3636b75adbd3c776b0dba4072e58e0dd83b6b87e
lushun_mac <470651748@qq.com>
2020-08-28 10:30:07 +0800

    ad1_s_decode

--- train_cityscapes.sh
+++ train_cityscapes.sh
@@ -2,13 +2,18 @@ if [ ! -d logdir  ];then
   mkdir logdir
 fi
 
+#************** search **************#
 #CUDA_VISIBLE_DEVICES=6 python train_autodeeplab.py \
 # --batch-size 2 --dataset cityscapes --checkname July29_newbetas_branch \
 # --alpha_epoch 20 --filter_multiplier 8 --resize 512 --crop_size 321
 
-CUDA_VISIBLE_DEVICES=5 nohup python -u train_autodeeplab.py --batch-size 2 --dataset cityscapes \
- --checkname ad0_s --alpha_epoch 20 --filter_multiplier 8 --resize 512 --crop_size 321  > logdir/ad0_s.log  2>&1 &
+#CUDA_VISIBLE_DEVICES=5 nohup python -u train_autodeeplab.py --batch-size 2 --dataset cityscapes \
+# --checkname ad0_s --alpha_epoch 20 --filter_multiplier 8 --resize 512 --crop_size 321  > logdir/ad0_s.log  2>&1 &
 
-CUDA_VISIBLE_DEVICES=6 nohup python -u train_autodeeplab.py --batch-size 4 --dataset cityscapes \
- --checkname ad1_s --alpha_epoch 20 --filter_multiplier 8 --resize 512 --crop_size 321  > logdir/ad1_s.log  2>&1 &
+#CUDA_VISIBLE_DEVICES=6 nohup python -u train_autodeeplab.py --batch-size 4 --dataset cityscapes \
+# --checkname ad1_s --alpha_epoch 20 --filter_multiplier 8 --resize 512 --crop_size 321  > logdir/ad1_s.log  2>&1 &
+
+#************** decode **************#
+CUDA_VISIBLE_DEVICES=6 nohup python -u decode_autodeeplab.py --dataset cityscapes \
+  --resume ./run/cityscapes/ad1_s/experiment_0/checkpoint.pth.tar > logdir/ad1_s_decode.log  2>&1 &
 
